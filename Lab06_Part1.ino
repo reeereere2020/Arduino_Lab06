2a2571242c0e3e25ff995c578f28eb82f4931b8d
reeereere2020 <61824449+reeereere2020@users.noreply.github.com>
2020-05-05 14:21:06 +0800

    Add files via upload

---
+++ Lab06_Part1.ino
@@ -0,0 +1,34 @@
+#include "pitches.h"
+
+int tune[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_C4,
+              NOTE_C4, NOTE_D4, NOTE_E4, NOTE_C4,
+              NOTE_E4, NOTE_F4, NOTE_G4,
+              NOTE_E4, NOTE_F4, NOTE_G4, 0,
+              NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
+              NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
+              NOTE_D4, NOTE_G3, NOTE_C4,
+              NOTE_D4, NOTE_G3, NOTE_C4};
+              
+float duration[] = {0.2, 0.2, 0.2, 0.2,
+                    0.2, 0.2, 0.2, 0.2,
+                    0.2, 0.2, 0.4, 0.2, 0.2, 0.4, 0.001,
+                    0.132, 0.067, 0.132, 0.067, 0.2, 0.2,
+                    0.132, 0.067, 0.132, 0.067, 0.2, 0.2,
+                    0.2, 0.2, 0.4, 0.2, 0.2, 0.4};              
+float length = sizeof(tune) / sizeof(tune[0]);
+
+void setup() {
+    pinMode(5, OUTPUT); 
+}
+
+void loop() {
+  for (int x=0; x<length; x++) {
+    tone(5,tune[x]);
+    delay(2500 * duration[x]); 
+    noTone(5);
+  }
+  delay(5000);
+}
+
+
+
---
+++ Lab06_Part2.ino
@@ -0,0 +1,39 @@
+#include "pitches.h"
+
+int tune4[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4};
+int tune5[] = {NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5};
+
+#include <Keypad.h>
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+  {'C', 'D', 'E', 'F'}, {'9','6','3', 'B'},
+  {'8', '5', '2', 'A'}, {'7', '4', '1', '0'}
+};
+byte rowPins[ROWS] = {16, 15, 14, 13};
+byte colPins[COLS] = {12, 11, 10, 9};
+Keypad keypad = Keypad( makeKeymap(keys), rowPins,colPins, ROWS, COLS);
+
+int index;
+char key;
+
+void setup() {
+    pinMode(5, OUTPUT);
+}
+
+void loop() {
+  key = keypad.getKey();
+  if((key-'1')>=0 && (key-'7')<=0) {
+    index = key - '1';
+    if(key != NO_KEY) {
+      tone(5,tune4[index],500);
+    }
+  }
+  else if((key-'A')>=0 && (key-'F')<=0) {
+    index = key - 'A';
+    if(key != NO_KEY) {
+      tone(5,tune5[index],500);
+    }
+  }
+}
+
